351a090aa79f97ab286d97a386335a901db26363
Tana Chanchiew <terktana18@gmail.com>
2021-04-06 21:43:59 +0700

    add random gifted perk

--- Assets/Scripts/Perks/PerkRepository.cs
+++ Assets/Scripts/Perks/PerkRepository.cs
@@ -12,12 +12,59 @@ public class PerkRepository : MonoSingleton<PerkRepository>
     [SerializeField]
     private List<Perk> wearablePerk;
 
-    public Perk GetRandomGiftedPerk()
+    public static List<Perk> GetRandomGiftedPerk()
     {
+        float[] probNumOfPerk = {0.15F, 0.95F, 1.0F};
         System.Random rnd = new System.Random();
-        int idx = -1;
-        idx = rnd.Next(giftedPerk.Count);
-        return giftedPerk[idx];
-           
+
+        int sumOfChance = 0;
+        for(int i = 0; i < Instance.giftedPerk.Count; i++)
+        {
+            sumOfChance += Instance.giftedPerk[i].Chance;
+        }
+        
+        List<float> probGiftedPerk = new List<float>();
+        float cumuProb = 0;
+        for(int i = 0; i < Instance.giftedPerk.Count; i++)
+        {
+            cumuProb += (float)Instance.giftedPerk[i].Chance / (float)sumOfChance;
+            probGiftedPerk.Add(cumuProb);
+        }
+
+        float prob = (float)rnd.NextDouble();
+        int numberOfPerk = 0;
+        for(int i = 0; i < probNumOfPerk.Length; i++)
+        {
+            if(prob < probNumOfPerk[i]) 
+            {
+                numberOfPerk = i + 1;
+                break;
+            }
+            
+        }
+
+        int loopTime = 0;
+        int maxLoopTime = 10000;
+        List<Perk> playerPerk = new List<Perk>();
+        while(numberOfPerk > 0)
+        {
+            double perkProb = rnd.NextDouble();
+            for(int i = 0; i < probGiftedPerk.Count; i++)
+            {
+                if(perkProb < probGiftedPerk[i] && !playerPerk.Contains(Instance.giftedPerk[i])) 
+                {
+                    playerPerk.Add(Instance.giftedPerk[i]);
+                    numberOfPerk -= 1;
+                    break;
+                }
+            }
+            loopTime += 1;
+            if(loopTime == maxLoopTime)
+            {
+                break;
+            }
+        }
+
+        return playerPerk;
     }
 }
