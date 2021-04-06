c95de1c964b55d393105c7d5b54f42244271f1d4
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2021-04-06 19:04:37 +0700

    Item color effect on rarity

--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -7,7 +7,12 @@ public class ItemPickup : Interactable
     [SerializeField]
     SpriteRenderer spriteRenderer;
     [SerializeField]
+    SpriteRenderer lightFromGround;
+    [SerializeField]
     private Item item;
+    private Color normalItemColor = new Color(0.9f, 0.9f, 0.9f);
+    private Color rareItemColor = new Color(0.0f, 0.9f, 0.1f);
+    private Color ultraRareItemColor = new Color(0.1f, 0.3f, 1.0f);
 
     public override void Interact()
     {
@@ -18,6 +23,20 @@ public class ItemPickup : Interactable
     public void AssignItem(Item item)
     {
         this.item = item;
+        switch (item.rarity)
+        {
+            case ItemRarity.Common:
+                lightFromGround.color = normalItemColor;
+                break;
+            case ItemRarity.Rare:
+                lightFromGround.color = rareItemColor;
+                break;
+            case ItemRarity.UltraRare:
+                lightFromGround.color = ultraRareItemColor;
+                break;
+            default:
+                throw new System.IndexOutOfRangeException($"Unhandled enum ItemRarity: {item.rarity}");
+        }
         EnableItem();
     }
 
